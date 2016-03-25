1318619274930b3db78a728aa6295939cadeea0b
Jon Agirre <jon.agirre@york.ac.uk>
2016-03-25 20:00:54 +0000

    WIP fragment kicks

--- clipper_tools/xray/structure_factors.py
+++ clipper_tools/xray/structure_factors.py
@@ -14,29 +14,29 @@ def structure_factors ( fsigf=None, hklinfo=None, mmol=None, bulk_solvent=True )
     log_string += "\n     bulk_solvent: %s" % bulk_solvent
 
     xml_root = etree.Element('structure_factors')
-    xml_root.attrib['bulk_solvent'] = str(bulk_solvent)
+    xml_root.attrib['bulk_solvent'] = str ( bulk_solvent )
 
     crystal = clipper.MTZcrystal()
     atoms = mmol.atom_list()
     
-    fc = clipper.HKL_data_F_phi_float( hklinfo, crystal )
+    fc = clipper.HKL_data_F_phi_float ( hklinfo, crystal )
     
     if bulk_solvent :
         sfcb = clipper.SFcalc_obs_bulk_float()
-        sfcb(fc, fsigf, atoms)
+        sfcb ( fc, fsigf, atoms )
         
         bulkfrc = sfcb.bulk_frac();
         bulkscl = sfcb.bulk_scale();
         
-        etree.SubElement(xml_root, 'bulk_fraction').text = str (bulkfrc)
-        etree.SubElement(xml_root, 'bulk_scale').text   = str (bulkscl)
+        etree.SubElement(xml_root, 'bulk_fraction').text = str ( bulkfrc )
+        etree.SubElement(xml_root, 'bulk_scale').text    = str ( bulkscl )
         
         log_string += "\n    bulk_fraction: %f " % bulkfrc
         log_string += "\n    bulk_scale: %f " % bulkscl
     
     else :
         sfc = clipper.SFcalc_obs_base_float()
-        sfc(fc, fsigf, atoms)
+        sfc ( fc, fsigf, atoms )
     
     log_string += "\n  << structure_factors has finished\n"
     xml_root.attrib['ok']    = 'yes'
